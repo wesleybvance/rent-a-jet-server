4f34ec4776755316c56b5040b8247eea525c7da0
Wesley Vance <wslyvnce@gmail.com>
2023-08-05 11:59:33 -0500

    updating register user for register form

--- rentajetapi/views/auth.py
+++ rentajetapi/views/auth.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rentajetapi.models.customer import Customer
+from rentajetapi.models.airport import Airport
+from rest_framework import status
+from rentajetapi.views.customer import CustomerSerializer
 
 
 @api_view(['POST'])
@@ -43,26 +46,42 @@ def register_user(request):
       request -- The full HTTP request object
     '''
 
-    # Now save the customer info
-    customer = Customer.objects.create(
-        uid=request.data['uid'],
-        first_name = request.data["firstName"],
-        last_name = request.data["lastName"],
-        email = request.data["email"],
-        profile_image = request.data["profileImage"],
-        phone_number = request.data["phoneNumber"],
-        home_airport = request.data["homeAirport"],
-    )
+    # # Now save the customer info
+    # customer = Customer.objects.create(
+    #     uid=request.data['uid'],
+    #     first_name = request.data["firstName"],
+    #     last_name = request.data["lastName"],
+    #     email = request.data["email"],
+    #     profile_image = request.data["profileImage"],
+    #     phone_number = request.data["phoneNumber"],
+    #     home_airport = Airport.objects.get(pk=request.data["homeAirport"])
+    # )
 
-    # Return the customer info to the client
-    data = {
-            'id': customer.id,
-            'uid': customer.uid,
-            'first_name': customer.first_name,
-            'last_name': customer.last_name,
-            'email': customer.email,
-            'profile_image': customer.profile_image,
-            'phone_number': customer.phone_number,
-            'home_airport': customer.home_airport
-    }
-    return Response(data)
+    # # Return the customer info to the client
+    # data = {
+    #         'id': customer.id,
+    #         'uid': customer.uid,
+    #         'first_name': customer.first_name,
+    #         'last_name': customer.last_name,
+    #         'email': customer.email,
+    #         'profile_image': customer.profile_image,
+    #         'phone_number': customer.phone_number,
+    #         'home_airport': customer.home_airport
+    # }
+    uid = request.META["HTTP_AUTHORIZATION"]
+    airport = Airport.objects.get(pk=request.data["homeAirport"])
+        
+    customer = Customer(
+        uid = uid,
+        first_name = request.data['firstName'],
+        last_name = request.data['lastName'],
+        email = request.data['username'],
+        phone_number = request.data['phoneNumber'],
+        profile_image = request.data['profileImage'],
+        home_airport = airport,
+        )
+        
+    serializer = CustomerSerializer(customer)
+    serializer.is_valid(raise_exception=True)
+    serializer.save(uid=uid)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
